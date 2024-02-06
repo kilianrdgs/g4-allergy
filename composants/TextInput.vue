fa8e7d99932475ebae069a608abcd67b139413b0
julienqtx <julien.quatravaux@live.fr>
2024-02-06 23:36:55 +0100

    formulaire créé

---
+++ composants/TextInput.vue
@@ -0,0 +1,48 @@
+<template>
+
+    <div>
+        
+        <form @submit.prevent="submitForm">
+        <label for="allergyInput">Entrer votre allergie :</label>
+        <input type="text" id="allergyInput" v-model="allergy" placeholder="Entrer votre Allergie" required>
+        
+        <fieldset>
+            <legend>Visibilité de l'allergie :</legend>
+            <label>
+            <input type="radio" v-model="visibility" value="public"> Publique
+            </label>
+            <label>
+            <input type="radio" v-model="visibility" value="private"> Privée
+            </label>
+        </fieldset>
+        
+        <button type="submit">Soumettre</button>
+        </form>
+    </div>
+
+</template>
+    
+<script>
+
+export default {
+    data() {
+        return {
+        allergy: '',
+        visibility: 'public'
+        }
+    },
+    methods: {
+        submitForm() {
+        // console.log('Allergie soumise :', this.allergy);
+        // console.log('Visibilité de l\'allergie :', this.visibility);
+        this.allergy = '';
+        this.visibility = 'public';
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/formulaire/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Formulaire</h1>
+
+        <TextInput v-model="textInput" placeholder="Entrer votre Allergie" />
+
+    </div>
+</template>
+    
+<script>
+    import TextInput from '@/composants/TextInput.vue'
+
+export default {
+  components: {
+    TextInput
+  },
+  data() {
+    return {
+      textInput: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
