c4d9a7434973967fd7e6b787774af314b2eb49b1
kilianrdgs <kilian.rdgs@free.fr>
2024-02-07 21:47:13 +0100

    formulaire inscription et design

--- pages/connexion/index.vue
+++ pages/connexion/index.vue
@@ -1,20 +1,22 @@
 <template>
-   <div>
-     <h1>Connexion</h1>
-     <form @submit.prevent="connexion">
-       <div>
-         <label for="email">Email:</label>
-         <input type="email" id="email" v-model="email" required>
+  <div class="page">
+    <div class="formulaire">
+     <h2>Connexion</h2>
+     <form @submit.prevent="connexion" style="display: flex; flex-direction: column; gap: 30px;">
+       <div class="inputs">
+         <input type="email" id="email" placeholder="adresse email" v-model="email" required>
+         <input type="password" id="password"  placeholder="mot de passe" v-model="password" required>
        </div>
-       <div>
-         <label for="password">Mot de passe:</label>
-         <input type="password" id="password" v-model="password" required>
-       </div>
-       <button type="submit">Se connecter</button>
+       <button type="submit" class="btn">Se connecter</button>
      </form>
+     <p>pas encore de compte ? <NuxtLink to="/inscription" style="color: blue; background-color: rgb(70, 199, 0); padding: 5px;">inscription</NuxtLink></p>
    </div>
+  </div>
+   
  </template>
  
+
+
  <script>
  export default {
    name: "Connexion",
@@ -56,6 +58,55 @@
  };
  </script>
  
+
  <style scoped>
+ .page{
+   height: 100vh;
+   width: 100vw;
+   display: flex;
+ }
+
+ h2{
+   margin-left: 20px;
+   margin-top: 10px;
+ }
+ .formulaire{
+   margin: auto;
+   background-image: url('/textures.webp');
+   width: 300px;
+   height: 300px;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   box-shadow: 21px 5px 15px 15px #3299aec8;
+ }
+
+ .inputs{
+   margin-left: 30px;
+   margin-right: 30px;
+   display: flex;
+   flex-direction: column;
+   gap: 10px;
+ }
+
+ .inputs input {
+   height: 25px;
+   background-color: yellow;
+ }
+
+ .btn{
+   margin: auto;
+   height: 30px;
+   width: 90px;
+   background-color: black;
+ }
+
+ p{
+   margin-left: auto;
+   margin-right: auto;
+   margin-bottom: 20px;
+
+}
  </style>
+ 
  
