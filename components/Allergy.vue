900fcb0dc4611249bc364792357a83fddf4f117c
BASTIEN VITOUR <bastien.vitour@edu.esiee-it.fr>
2024-02-07 11:12:21 +0100

    récupération liste allergies

---
+++ components/Allergy.vue
@@ -0,0 +1,17 @@
+<template>
+    <h4>{{ allergy.createdBy.name }}</h4>
+    <span>{{ allergy.name }}</span>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "Allergy",
+        props: { 
+            allergy: Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
