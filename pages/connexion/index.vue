ba20707149476978c27428172616a91df6f0ada3
Adam <adamboissy2004@gmail.com>
2024-02-07 11:08:27 +0100

    connexion fonctionnelle

--- pages/connexion/index.vue
+++ pages/connexion/index.vue
@@ -1,13 +1,61 @@
 <template>
-    <h1>Connexion</h1>
-    </template>
-    
-    <script>
-     export default {
-        name: "connexion"
-     }
-    </script>
-    
-    <style>
-    
-    </style>
+   <div>
+     <h1>Connexion</h1>
+     <form @submit.prevent="connexion">
+       <div>
+         <label for="email">Email:</label>
+         <input type="email" id="email" v-model="email" required>
+       </div>
+       <div>
+         <label for="password">Mot de passe:</label>
+         <input type="password" id="password" v-model="password" required>
+       </div>
+       <button type="submit">Se connecter</button>
+     </form>
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+   name: "Connexion",
+   data() {
+     return {
+       email: "",
+       password: "",
+     };
+   },
+   methods: {
+  async connexion() {
+    try {
+      console.log(this.email)
+      console.log(this.password)
+      const response = await fetch('http://localhost:3001/connexion', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: this.email,
+          password: this.password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Erreur lors de la connexion');
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      console.error('Erreur lors de la connexion:', error.message);
+    }
+  },
+},
+
+ };
+ </script>
+ 
+ <style scoped>
+ </style>
+ 
